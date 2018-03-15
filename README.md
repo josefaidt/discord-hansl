3c8cccfc6fe05b59baaff057242f06142020c3d5
josef <josef.aidt@gmail.com>
2018-03-14 20:48:38 -0500

    added commands

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Janus
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com)
+
 A Discord bot built on Node.js using [discordjs](https://discord.js.org/#/) interfacing with cryptocurrency exchanges and as an all-around housebot.
 
 ## Early Stages
-For information regarding the status of this project, please visit the [trello board](https://trello.com/b/DiDh1BRb) or the [changelog](https://github.com/josefaidt/Janus/blob/master/CHANGELOG.md).
+For information regarding the status of this project, please visit the [trello board](https://trello.com/b/DiDh1BRb) or the [changelog](https://github.com/josefaidt/Janus/blob/master/CHANGELOG.MD).
 
 ## The Plan
 The initial thought was to begin interfacing the bot to GDAX and Binance/Bittrex API to report major changes, fluctuations, and exchange statuses of the crypto market to my personal discord server. As the bot grows I will be sure to add additional, miscellaneous features.
--- bot.js
+++ bot.js
@@ -1,60 +1,46 @@
-"use strict";
-
-// load Node libraries
-const http = require("http");
-const https = require("https");
+'use strict'
 
 // load and initialize Discord.js library
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+const Discord = require('discord.js')
+const bot = new Discord.Client()
 // The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'Mzk1OTczODkyOTkwMDQyMTEz.DSaq9A.PQMvswFW-NbIue81LBzkV-PzjIo';
-
-// load and initialize GDAX library
-const Gdax = require("gdax");
-const publicClient = new Gdax.PublicClient();
-const gdaxAPISandbox = "https://api-public.sandbox.gdax.com";
-const gdaxAPI = "https://api.gdax.com";
+const token = 'Mzk1OTczODkyOTkwMDQyMTEz.DSaq9A.PQMvswFW-NbIue81LBzkV-PzjIo'
 
 // load user libraries
-const Commands = require("./lib/commands.js");
+const Commands = require('./lib/commands.js')
 
-function hasCommand(value) {
-  return Object.keys(Commands).some( key => Commands[key].name == value);
+function hasCommand (value) {
+  return Object.keys(Commands).some(key => Commands[key].name === value)
 }
 
-
 bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.tag}!`);
-});
+  console.log(`Logged in as ${bot.user.tag}!`)
+})
 
 bot.on('message', msg => {
-  const cmdPrefix = '$';
-  if (msg.author.id != bot.user.id && msg.content[0] === cmdPrefix) {
-
-    let cmdText = msg.content.split(" ")[0].substring(1).toLowerCase();
-    let suffix = msg.content.substring(cmdText.length + 2); //add one for the $ and one for the space
+  const cmdPrefix = '$'
+  if (msg.author.id !== bot.user.id && msg.content[0] === cmdPrefix) {
+    let cmdText = msg.content.split(' ')[0].substring(1).toLowerCase()
+    let suffix = msg.content.substring(cmdText.length + 2) // add one for the $ and one for the space
 
     if (hasCommand(cmdText)) {
-      let cmd = Commands[cmdText];
+      let cmd = Commands[cmdText]
 
-      if (cmd.name == 'help') {
-
+      if (cmd.name === 'help') {
         // use standard msg.process for now, more checking eventually
-        cmd.process(bot, msg, suffix);
+        cmd.fn(msg, suffix)
       } else {
-        cmd.process(bot, msg, suffix);
+        cmd.fn(msg, suffix)
       }
     } else {
-      msg.channel.send("Oops, don't know that one.");
+      msg.channel.send("Oops, don't know that command.")
     }
-    
   }
 
   // for testing...
   if (msg.content === 'ping') {
-    msg.reply('Pong!');
+    msg.reply('Pong!')
   }
-});
+})
 
-bot.login(token);
+bot.login(token)
